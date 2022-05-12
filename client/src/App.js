e6d65acf49759dbf53c61c28987913ae246ebdf5
buraksaler <burakksaler@gmail.com>
2022-05-12 16:52:50 +0300

    add client and Booklist component

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+
+
+//Components
+import BookList from "./components/BookList";
+
+
+function App() {
+  return (
+    <div id="main">
+      <h1>Burak's Reading List</h1>
+      <BookList />
+    </div>
+  );
+}
+
+export default App;
